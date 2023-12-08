f07a300de137c143781f4688f9400f982597a051
yeswanth2005 <147792079+yeswanth2005@users.noreply.github.com>
2023-12-08 09:40:14 +0530

    #include<stdio.h> int add(int,int); int main() {     int a,b;     scanf("%d%d",&a,&b);     int res=add(a,b);     printf("%d",res); } int add(int a,int b) {     int c;     c=a+b;     return c;
    }

---
+++ Adding_Two_Numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int add(int,int);
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int res=add(a,b);
+    printf("%d",res);
+}
+int add(int a,int b)
+{
+    int c;
+    c=a+b;
+    return c;
+    
+}
