5554affe6138f29341bde8a68bf3d0d5f8d76d2b
yeswanth2005 <147792079+yeswanth2005@users.noreply.github.com>
2024-01-04 19:10:58 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j==i || j==n-i+1)             {                 printf("%c",64+i);             }             else{                 printf(" ");             }         }         printf(" ");     } }

---
+++ Pattern_8.4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j==i || j==n-i+1)
+            {
+                printf("%c",64+i);
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("
+");
+    }
+}
